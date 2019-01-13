6edc3b6be8cffa791eb250860917d511ee325942
Dave James Miller <dave@davejamesmiller.com>
2019-01-13 16:53:50 +0000

    Add X Server

--- .bash/windows.bash
+++ .bash/windows.bash
@@ -1,9 +1,8 @@
 if $WINDOWS; then
 
-    # Clear the MSysGit MOTD (at this point it's already been shown, but this
-    # gets rid of it for the future)
-    if [ -w /etc/motd ]; then
-        > /etc/motd
+    # Set the X display, since Cygwin doesn't do it automatically
+    if [ -z "$DISPLAY" ]; then
+        export DISPLAY=:0
     fi
 
     # Emulate apt-get with apt-cyg
@@ -16,5 +15,11 @@ if $WINDOWS; then
 
     # Expand aliases after 'winpty'
     alias winpty='winpty '
+
+    # Clear the MSysGit MOTD (at this point it's already been shown, but this
+    # gets rid of it for the future)
+    if [ -w /etc/motd ]; then
+        > /etc/motd
+    fi
 
 fi
