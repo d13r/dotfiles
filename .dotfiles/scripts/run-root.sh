9e2d5e3349b2271c9ccb1e7472d2360df07962b6
Dave James Miller <dave@davejamesmiller.com>
2019-01-31 22:00:38 +0000

    Move 'cfg' into this repo, and improve the Docker test environment

--- .dotfiles/scripts/run-root.sh
+++ .dotfiles/scripts/run-root.sh
@@ -3,14 +3,14 @@ set -o nounset -o pipefail -o errexit
 cd "$(dirname "$0")/.."
 
 ################################################################################
-# Run the setup script as a regular user.
+# Run the setup script as the root user.
 ################################################################################
 
 # Rebuild the image if the 'cfg' script changes (if not it's cached so this is quick)
-scripts/build.sh
+scripts/_build.sh
 
 source ~/.bash/000-vars.bash
 source ~/.bash/docker.bash
 
 # Use SSH with agent forwarding so we can commit changes made inside Docker
-dsh dotfiles
+dsh dotfiles /bin/bash -u root
