00918282eaacc79104cddfcfcdf0812bfd02d11c
Dave James Miller <dave@davejamesmiller.com>
2019-07-21 19:51:58 +0100

    Shortcut 'redis 1' -> 'redis-cli -n 1'

--- .bash/redis.bash
+++ .bash/redis.bash
@@ -1,1 +1,9 @@
-alias redis='redis-cli'
+redis()
+{
+    if [[ $# -eq 1 ]] && [[ $1 =~ ^[0-9]+$ ]]; then
+        # e.g. "redis 1"
+        redis-cli -n "$1"
+    else
+        redis-cli "$@"
+    fi
+}
