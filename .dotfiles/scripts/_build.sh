9a41ed7b5a53d35333e68972402223bfd66b2a23
Dave James Miller <d@djm.me>
2023-01-27 22:34:42 +0000

    Fix running Dotfiles Docker helper scripts with 't'; Remove old 't' script help text

--- .dotfiles/scripts/_build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit -o nounset -o pipefail
-cd "$(dirname "$0")/.."
-
-################################################################################
-# Build Docker image.
-################################################################################
-
-exec docker build -t dotfiles .
